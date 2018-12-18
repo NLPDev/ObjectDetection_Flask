b4e133e1131b87fed82712d6c4b18133089ef322
ML expert <vasile123andronic@gmail.com>
2018-12-18 09:24:50 -0800

    util

--- utils/label_map_util.py
+++ utils/label_map_util.py
@@ -123,6 +123,16 @@ def load_labelmap(path):
   _validate_label_map(label_map)
   return label_map
 
+  with tf.gfile.GFile(path, 'r') as fid:
+    label_map_string = fid.read()
+    label_map = string_int_label_map_pb2.StringIntLabelMap()
+    try:
+      text_format.Merge(label_map_string, label_map)
+    except text_format.ParseError:
+      label_map.ParseFromString(label_map_string)
+  _validate_label_map(label_map)
+  return label_map
+
 
 def get_label_map_dict(label_map_path):
   """Reads a label map and returns a dictionary of label names to id.
